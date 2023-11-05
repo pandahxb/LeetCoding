d020ba5084971c537b60d462e2193123cebdba7b
Kaifeng Li <kaifeng-li@outlook.com>
2023-11-05 18:12:17 -0500

    add: 605.py

---
+++ py/605.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):  # O(n)
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (
+                    i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+        return False
+
+    # Time: O(n); Space: O(1)
