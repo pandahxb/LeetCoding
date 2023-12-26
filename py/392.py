f8b6b345caddbaf3897a02c436f0a951f6efdcc0
kaifengli <kaifeng-li@outlook.com>
2023-12-26 11:44:32 -0500

    Accepted

---
+++ py/392.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if s == "":
+            return True
+        i = 0
+        length = len(s)
+        for char in t:
+            if s[i] == char:
+                i += 1
+                if i == length:
+                    return True
+        return False
