93705a2e977769c82c65e182faf895b803555b69
kaifengli <kaifeng-li@outlook.com>
2023-12-27 20:30:04 -0500

    Accepted

---
+++ py/1456.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        maxVowels = 0
+        vowels = set('aeiou')
+        for i in range(k):
+            if s[i] in vowels:
+                maxVowels += 1
+        currentVowels = maxVowels
+        for i in range(1, len(s) - k + 1):
+            if s[i - 1] in vowels:
+                currentVowels -= 1
+            if s[i - 1 + k] in vowels:
+                currentVowels += 1
+            maxVowels = max(maxVowels, currentVowels)
+        return maxVowels
