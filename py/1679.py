af8640ce0ebe9f13420e5e93cba7d68485fab110
kaifengli <kaifeng-li@outlook.com>
2023-12-27 19:05:04 -0500

    Accepted

---
+++ py/1679.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        op = 0
+        left, right = 0, len(nums) - 1
+        nums.sort()
+        while nums[right] > k and right != 0:
+            right -= 1
+        while left < right:
+            if nums[left] + nums[right] == k:
+                left += 1
+                right -= 1
+                op += 1
+                continue
+            if k - nums[left] > nums[right]:
+                left += 1
+            else:
+                right -= 1
+        return op
