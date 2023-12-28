2f273ff530c49ee9154fae6fd2c158ce673fcd5b
kaifengli <kaifeng-li@outlook.com>
2023-12-27 20:08:29 -0500

    Accepted

---
+++ py/643.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        maxSum = currentSum = sum(nums[:k])
+        for i in range(1, len(nums) - k + 1):
+            currentSum += nums[i - 1 + k] - nums[i - 1]
+            maxSum = max(maxSum, currentSum)
+        return maxSum / k
