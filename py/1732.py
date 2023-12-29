48a45104607ca310b69d8dddd11d9857622763f2
kaifengli <kaifeng-li@outlook.com>
2023-12-29 12:55:18 -0500

    Accepted

---
+++ py/1732.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        highest = current = 0
+        for i in range(len(gain)):
+            current += gain[i]
+            highest = max(highest, current)
+        return highest
