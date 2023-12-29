f8f1ec694f02fb5559a2e4ba7fd75a4f9960dbc7
kaifengli <kaifeng-li@outlook.com>
2023-12-29 13:09:02 -0500

    format

--- py/724.py
+++ py/724.py
@@ -7,7 +7,7 @@ class Solution:
         right = sum(nums)
         for i in range(len(nums)):
             right -= nums[i]
-            if (left == right):
+            if left == right:
                 return i
             left += nums[i]
         return -1
