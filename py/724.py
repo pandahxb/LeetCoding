9650808ebdad185572e9dbad83a647531da52004
kaifengli <kaifeng-li@outlook.com>
2023-12-29 13:07:39 -0500

    Accepted

---
+++ py/724.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left = 0
+        right = sum(nums)
+        for i in range(len(nums)):
+            right -= nums[i]
+            if (left == right):
+                return i
+            left += nums[i]
+        return -1
+
+# Time: O(n), Space: O(1)
