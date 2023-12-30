dfd68cbfa04f5990a3c7065a065daef3a71e2d8e
kaifengli <kaifeng-li@outlook.com>
2023-12-30 12:20:03 -0500

    Accepted

---
+++ py/2215.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        return [list(set(nums1) - set(nums2)), list(set(nums2) - set(nums1))]
+
+# Time: O(n), Space: O(n)
