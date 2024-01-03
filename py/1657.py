10ed106645013d2d617194b5791178360900860a
kaifengli <kaifeng-li@outlook.com>
2024-01-03 14:17:00 -0500

    Accepted

---
+++ py/1657.py
@@ -0,0 +1,12 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        occ1, occ2 = {}, {}
+        for i in word1:
+            occ1[i] = occ1.get(i, 0) + 1
+        for i in word2:
+            occ2[i] = occ2.get(i, 0) + 1
+        if set(occ1.keys()) == set(occ2.keys()) and sorted(occ1.values()) == sorted(occ2.values()):
+            return True
+        return False
+
+# Time: O(n), Space: O(n)
