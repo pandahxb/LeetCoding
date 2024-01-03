dc12b9e54b31a97a5ed9254b971146d35254833e
kaifengli <kaifeng-li@outlook.com>
2024-01-03 17:46:01 -0500

    Accepted

--- py/1657.py
+++ py/1657.py
@@ -1,12 +1,17 @@
+from collections import Counter
+
+
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        occ1, occ2 = {}, {}
-        for i in word1:
-            occ1[i] = occ1.get(i, 0) + 1
-        for i in word2:
-            occ2[i] = occ2.get(i, 0) + 1
-        if set(occ1.keys()) == set(occ2.keys()) and sorted(occ1.values()) == sorted(occ2.values()):
-            return True
-        return False
+        # occ1, occ2 = {}, {}
+        # for i in word1:
+        #     occ1[i] = occ1.get(i, 0) + 1
+        # for i in word2:
+        #     occ2[i] = occ2.get(i, 0) + 1
+        # if set(occ1.keys()) == set(occ2.keys()) and sorted(occ1.values()) == sorted(occ2.values()):
+        #     return True
+        # return False
+
+        return set(word1) == set(word2) and Counter(Counter(word1).values()) == Counter(Counter(word2).values())
 
 # Time: O(n), Space: O(n)
