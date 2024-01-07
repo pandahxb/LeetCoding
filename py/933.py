6c10208a378b227f321b3eb0d959fb09bf8f7166
kaifengli <kaifeng-li@outlook.com>
2024-01-07 13:26:24 -0500

    Accepted

---
+++ py/933.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+class RecentCounter:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        queue = self.queue
+        queue.append(t)
+        while queue and queue[0] < t - 3000:
+            queue.popleft()
+        return len(queue)
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
+
+# Time: O(N), Space: O(N)
